9c87c49a50d85a824ff8597a158ffda503fc4382
Owen Cole <ocole161@gmail.com>
2022-12-16 04:41:48 -0700

    commit tree 8575c1fce1ebffbfb7de3f7a6114ea959ab5de2d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+
+
+
+function submitData(name, email) {
+    let conObj = {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        })
+    }
+    return fetch('http://localhost:3000/users', conObj)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        document.body.innerHTML - object["id"]
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+}
